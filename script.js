f59fb4ecbe2d907583c4154a39be9d10c64ab2bf
Ahmed <104452787+0xcyborg@users.noreply.github.com>
2023-01-09 18:31:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 2023 01 08
+// 2023,01,08
 
 document.getElementById("content").style.display = "inline-block";
 
@@ -7,26 +7,16 @@ Count = 0;
 Play = true;
 Cases = [ [1,4,7],[2,5,8],[3,6,9],[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7] ];
 
-function Show_Button(){
-    document.getElementById("refresh").style.display = "block";
-}
-
 function Main(Element){
     if(Element.src.includes("default") && Play){
+        if(Turn == 0){ Element.src = "files/0.png"; Turn = 1 }
+        else{ Element.src = "files/1.png"; Turn = 0 }
         Count += 1;
-        if(Turn == 0){
-            Element.src = "files/0.png";
-            Turn = 1;
-        }
-        else{
-            Element.src = "files/1.png";
-            Turn = 0;
-        }
-        Win();
+        Check_Winner();
     }
 }
 
-function Win(){
+function Check_Winner(){
     for(I = 0; I < Cases.length; I++){
         var Ref;
         for(J = 0; J < 3; J++){
@@ -37,18 +27,19 @@ function Win(){
             else if(!Src.includes("default") && J == 2) After_Win(Src);
         }
     }
-    if(Count == 9 && Play == true){
-        document.getElementById("result").innerHTML = "Tie";
-        Show_Button();
-    }
+    if(Count == 9 && Play == true){ document.getElementById("result").innerHTML = "Tie"; Game_Over() }
 }
 
-async function After_Win(Src){
+function After_Win(Src){
+    Src.includes("0.png") ? document.getElementById("result").innerHTML = "Player 'X' Win" : document.getElementById("result").innerHTML = "Player 'O' Win";
+    Play = false;
+    Game_Over();
+}
+
+async function Game_Over(){
     Sound = new Audio('files/sound.mp3');
     Sound.play();
-    Show_Button();
-    Play = false;
-    Src.includes("0.png") ? document.getElementById("result").innerHTML = "Player X Win" : document.getElementById("result").innerHTML = "Player O Win";
+    document.getElementById("refresh").style.display = "block";
     for(Num = 0; Num < 256; Num++){
         document.body.style.background = (`rgb(${Num},${Num},${Num + 50})`);
         await new Promise(r => setTimeout(r, 100));
